beb8c1512ce67e73bc1af6325a3b5e9c48577237
kaspar muuli <kasparmuuli@gmail.com>
2022-03-09 15:22:56 +0200

    add new project.

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis on ta mängu võitunud;
+            //katsete arv on piiramatu.
+            //*programm genereerib juhuslikku numbrit ühte korda.
+
+            bool isCorrectGuess = false;
+            Random rnd = new Random();
+
+            int cpuNumber = rnd.Next(1, 11);
+            int i = 0;
+
+            Console.WriteLine("Tere tulemast numbri äraarvamise mängu");
+            Console.WriteLine("Number 1 kuni 10ni genereeritakse");
+            Console.WriteLine("Kui sa arvad õige numbri ära, oled võitnud!");
+
+            while (!isCorrectGuess)
+            {
+                Console.WriteLine("Paku oma vastus.");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess > cpuNumber)
+                {
+                    Console.WriteLine("Sinu vastus on liiga kõrge");
+                }
+                else if(guess < cpuNumber)
+                {
+                    Console.WriteLine("Sinu vastus on liiga madal");
+                }
+                else
+                {
+                    i++;
+                    Console.WriteLine("Õige");
+                    isCorrectGuess = true;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis on ta mängu võitunud;
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme katsega numbrit ära arvata
+            //siis mängu võidab arvuti.
+            //*programm genereerib juhuslikku numbrit ühe korra.
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+            int i = 0;
+
+            Console.WriteLine("Sul on 3 katset, et numbrit ära arvata");
+
+            while (i < 3)
+            {
+                string guess = Console.ReadLine();
+                int guessedNumber = Convert.ToInt32(guess);
+
+                if(guessedNumber != cpuNumber)
+                {
+                    Console.WriteLine("Proovi uuesti!");
+
+                    i++;
+                }
+                else
+                    Console.WriteLine("Õige vastus!");
+                
+
+            }
+
+            Console.WriteLine("Mäng läbi!");
+
+        }
+    }
+}
